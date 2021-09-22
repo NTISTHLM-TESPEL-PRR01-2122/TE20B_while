a6e63b982ab586b619e11490462babc1b2243914
Mikael Bergström <krank23@gmail.com>
2021-09-22 10:26:56 +0200

    Initial commit

---
+++ TE20B_while/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+// int hp = 10;
+
+// while (hp > 0)
+// {
+//   hp--;
+//   Console.WriteLine(hp);
+// }
+
+// Console.ReadLine();
+
+
+
+// string name = "";
+
+// while (name != "micke" && name != "mikael")
+// {
+//   Console.WriteLine("Vad heter du?");
+//   name = Console.ReadLine();
+// }
+
+while (true)
+{
+  string answer;
+  answer = Console.ReadLine();
+
+  if (answer == "a" || answer == "b")
+  {
+    Console.WriteLine("rätt!");
+  }
+
+  // Console.ReadLine();
+}
+
+
+// if (name == "")
+// {
+//   Console.WriteLine("Vad heter du?");
+//   name = Console.ReadLine();
+
+//   if (name == "")
+//   {
+//     Console.WriteLine("Vad heter du?");
+//     name = Console.ReadLine();
+
+//     if (name == "")
+//     {
+//       Console.WriteLine("Vad heter du?");
+//       name = Console.ReadLine();
+//     }
+//   } 
+
+// }
